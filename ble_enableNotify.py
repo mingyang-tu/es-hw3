c4041525f9af2231c936f90364c49662df3f84c1
本 <hung@hung.local>
2023-03-30 12:05:14 +0800

    notifications

---
+++ ble_enableNotify.py
@@ -0,0 +1,67 @@
+# ble_scan_connect.py:
+from bluepy.btle import Peripheral, UUID
+from bluepy.btle import Scanner, DefaultDelegate
+
+
+class ScanDelegate(DefaultDelegate):
+    def __init__(self):
+        DefaultDelegate.__init__(self)
+
+    def handleDiscovery(self, dev, isNewDev, isNewData):
+        if isNewDev:
+            print("Discovered device", dev.addr)
+        elif isNewData:
+            print("Received new data from", dev.addr)
+    def handleNotification(self, cHandle, data):
+        print("A notification was received: %s" %data)
+        
+
+
+scanner = Scanner().withDelegate(ScanDelegate())
+devices = scanner.scan(10.0)
+n = 0
+addr = []
+for dev in devices:
+    print("%d: Device %s (%s), RSSI=%d dB" % (n, dev.addr, dev.addrType, dev.rssi))
+    addr.append(dev.addr)
+    n += 1
+    for (adtype, desc, value) in dev.getScanData():
+        print(" %s = %s" % (desc, value))
+
+number = input('Enter your device number: ')
+print('Device', number)
+num = int(number)
+print(addr[num])
+#
+print("Connecting...")
+dev = Peripheral(addr[num], 'random')
+#
+print("Services...")
+for svc in dev.services:
+    print(str(svc))
+#
+try:
+    testService = dev.getServiceByUUID(UUID(0xfff0))
+    for ch in testService.getCharacteristics():
+        print(str(ch))
+#
+    ch = dev.getCharacteristics(uuid=UUID(0xfff1))[0]
+    if (ch.supportsRead()):
+        print(ch.read())
+    cccd = ch.valHandle + 1
+    cccd.write("\x01\x00")
+    print(" Enabled notifications.")
+    i = 0
+    while True:
+        if dev.waitForNotifications(1.0):
+            #handleNotification() was called
+            i += 1
+            if (i > 1000):
+                break
+            continue
+
+        print("Waiting...")
+#
+
+finally:
+    dev.disconnect()
