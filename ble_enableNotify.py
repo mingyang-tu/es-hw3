f92fc366a511b09c76058eecc76cb1c16e499280
本 <hung@hung.local>
2023-03-31 00:47:51 +0800

    debug

--- ble_enableNotify.py
+++ ble_enableNotify.py
@@ -1,4 +1,3 @@
-# ble_scan_connect.py:
 from bluepy.btle import Peripheral, UUID
 from bluepy.btle import Scanner, DefaultDelegate
 
@@ -13,16 +12,13 @@ class ScanDelegate(DefaultDelegate):
         elif isNewData:
             print("Received new data from", dev.addr)
     def handleNotification(self, cHandle, data):
-        print("A notification was received: %s" %data)
-        
-
-
+        print("Received data: " + str(data))
 scanner = Scanner().withDelegate(ScanDelegate())
 devices = scanner.scan(10.0)
 n = 0
 addr = []
 for dev in devices:
-    print("%d: Device %s (%s), RSSI=%d dB" % (n, dev.addr, dev.addrType, dev.rssi))
+    print("%d: Device %s (%s), RSSI=%d dB" % (n,dev.addr, dev.addrType, dev.rssi))
     addr.append(dev.addr)
     n += 1
     for (adtype, desc, value) in dev.getScanData():
@@ -32,36 +28,36 @@ number = input('Enter your device number: ')
 print('Device', number)
 num = int(number)
 print(addr[num])
-#
+
 print("Connecting...")
 dev = Peripheral(addr[num], 'random')
-#
+
 print("Services...")
 for svc in dev.services:
     print(str(svc))
-#
-try:
+
+try :
     testService = dev.getServiceByUUID(UUID(0xfff0))
     for ch in testService.getCharacteristics():
         print(str(ch))
-#
+
     ch = dev.getCharacteristics(uuid=UUID(0xfff1))[0]
     if (ch.supportsRead()):
         print(ch.read())
+
+    print("Before enable notifications: " + ch.propertiesToString())
     cccd = ch.valHandle + 1
-    cccd.write("\x01\x00")
-    print(" Enabled notifications.")
+    dev.writeCharacteristic(cccd, b"\x01\x00")
+    print("Enabled notifications.")
+    print("After enable notifications: " + ch.propertiesToString())
     i = 0
     while True:
-        if dev.waitForNotifications(1.0):
-            #handleNotification() was called
-            i += 1
+        if dev.waitForNotifications(3.0):
+            i += 3
             if (i > 1000):
                 break
             continue
-
         print("Waiting...")
-#
 
 finally:
     dev.disconnect()
